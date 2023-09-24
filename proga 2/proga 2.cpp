7f14e15444164e3184af7796f68b00d61035f4fa
nonplex <fuksvladislav@gmail.com>
2023-09-25 01:21:24 +0700

    Добавьте файлы проекта.

---
+++ proga 2/proga 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+	using namespace std;
+	double x;
+	//Вещественные числа
+	cout << "x = "; cin >> x;
+	//Запрос х для универсальности
+	double y = 3 * pow(x, 2) - 6 * x - 7;
+	//Формула из задания
+	cout << "y = " << y << endl;
+	//Вывод ответа
+}
